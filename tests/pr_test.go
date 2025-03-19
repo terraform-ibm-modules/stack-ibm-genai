08893304a57c9f69e56cb511b2d893def26a19f7
Alex Reiff <alex.reiff@ibm.com>
2025-03-19 13:22:49 -0400

    switch default region to eu-de

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -1,9 +1,10 @@
 package tests
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testprojects"
-	"testing"
 )
 
 func TestProjectsFullTest(t *testing.T) {
